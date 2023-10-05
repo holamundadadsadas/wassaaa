bb1678071f4921f37c9b960f145a6f574bc9bbe2
holamundadadsadas <147024285+holamundadadsadas@users.noreply.github.com>
2023-10-05 00:14:04 -0500

    wassaaa

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import session from "express-session";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import passport from "./config/passposrt.js";
+import { UsersRoute } from "./routes/user.routes.js";
+import { PageRoute } from "./routes/page.routes.js";
+import { limitGrt } from "./config/limiter.js";
+import cors from "cors";
+import { decrypt } from "./version/extra/extras_actions.js";
+import { crearToken } from "./config/JWT.js";
+
+// Environment variables
+dotenv.config();
+
+// Initialize server
+const index = express();
+
+// Setting
+index.set("port", process.env.PORT || 3000);
+
+// Middlewares
+index.use(morgan("dev"));
+index.use(limitGrt())
+index.use(cors());
+index.use(express.json());
+index.use(
+  session({
+    secret: "log",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//index.use(limitGrt());
+index.use(passport.initialize());
+index.use(passport.session());
+
+// Essential Routes
+index.use("/token", crearToken);
+index.post("/encript", decrypt);
+index.get("/return", (req, res) => {
+  res.redirect("http://localhost:5173/");
+});
+
+//Page Routes
+index.use("/", PageRoute);
+
+//User Routes
+index.use("/", UsersRoute);
+
+// Server
+index.listen(index.get("port"), () => {
+  console.log("Server on port " + index.get("port"));
+});
